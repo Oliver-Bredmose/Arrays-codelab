ebbd4131eb0eace24548cbb99dfbe7764922dd13
Oliver-Bredmose <oliver.bredmose@gmail.com>
2025-08-25 13:41:31 +0200

    Console.log my arrays
    Console.log my arrays in order of opgave 2

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -2,7 +2,7 @@
 
 //Opgave 1
 const myArray = ["element1","element2","element3","element4","element5"]
-console.log (myArray)
+console.log(myArray)
 
 
 
@@ -15,3 +15,14 @@ let element3 = myArray[0];
 let element4 = myArray[3];
 let element5 = myArray[1];
 
+console.log(myArray[2])
+console.log(myArray[4])
+console.log(myArray[0])
+console.log(myArray[3])
+console.log(myArray[1])
+
+<li>element3</li>
+<li>element5</li>
+<li>element1</li>
+<li>element4</li>
+<li>element2</li>
