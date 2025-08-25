beba13d811f7469d71bb53585f0ffc07971ea961
Oliver-Bredmose <oliver.bredmose@gmail.com>
2025-08-25 13:32:18 +0200

    reagranged element
    reagranged element for opgave 2

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -1,17 +1,17 @@
 /* arrays */
 
 //Opgave 1
-const myArray = console.logs
-'element1'
-'element2'
-'element3'
-'element4'
-'element5';
+const myArray = ["element1","element2","element3","element4","element5"]
+console.log (myArray)
 
 
 
 
 // opgave 2 console.logs
 
-
+let element1 = myArray[2];
+let element2 = myArray[4];
+let element3 = myArray[0];
+let element4 = myArray[3];
+let element5 = myArray[1];
 
